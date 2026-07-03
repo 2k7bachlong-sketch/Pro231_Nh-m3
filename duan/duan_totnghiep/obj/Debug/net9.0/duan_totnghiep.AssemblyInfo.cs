edd458b2f03b7ec7d6a5ad1e525e0bb9f3ecf989
2k7bachlong <2k7bachlong@gmail.com>
2026-07-03 15:43:48 +0700

    CSS

--- duan/duan_totnghiep/obj/Debug/net9.0/duan_totnghiep.AssemblyInfo.cs
+++ duan/duan_totnghiep/obj/Debug/net9.0/duan_totnghiep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("duan_totnghiep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7e27afd29c76bb27795a643f4f77481155c519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8f6baa8ef1a08bbbc6e6183e0422e820f7f78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("duan_totnghiep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("duan_totnghiep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
